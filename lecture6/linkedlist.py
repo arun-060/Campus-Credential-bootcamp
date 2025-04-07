2b78eda8af4881302e4b8893ee2d39dabf9818a8
Arunkumar Gupta <guptaarunkumar060.com>
2025-04-07 10:45:38 +0530

    linked list completed

--- lecture6/linkedlist.py
+++ lecture6/linkedlist.py
@@ -34,7 +34,16 @@ class LinkedList:
             self.tail=node
         else :
             node.text=self.head
-            self.head = node
+            
+    def addNodeAtPos(self,value,position):
+        node = Node(value)
+        current = self.head
+        i = position-1
+        while i != 0:
+            current = current.next
+            i -= 1
+        node.next = current.next
+        current.next = node
         
 if __name__ == "__main__":
     object = LinkedList()
@@ -45,21 +54,35 @@ if __name__ == "__main__":
         print("3.Add node in between : ")
         print("4.Add node in end : " )
         print("5.Display linked list : ")
-        print("6.Exit")
+        print("6.Add node before position : ")
+        print("7.Add node after position : ")
+        print("8.Exit")
         
         ch = int(input("Enter your choice : "))
         if ch == 1:
             value = int(input("Enter the value for node : "))
             object.addNode(value)
             print("node added successfully in linked list: ")
+        elif ch == 3:
+            value = int(input("Enter the value for node : "))
+            position = int(input("Enter the position : "))
+            object.addNodeAtPos(value,position)
         elif ch == 5:
             object.display()
         elif ch == 2:
             value = int(input("Enter the value for node : "))
             object.addBegining(value)
             print("Value added at the begining")
+        elif ch == 4:
+            value = int(input("Enter the value of node : "))
+            object.addNode(value=value)
+        elif ch == 6:
+            value = int(input("Enter the value for node : "))
+            position = int(input("Enter the position : "))
+            object.addNodeAtPos(value,position-1) 
+        elif ch == 7:
+            value = int(input("Enter the value for node : "))
+            position = int(input("Enter the position : "))
+            object.addNodeAtPos(value,position+1) 
         else:
             sys.exit()
-    
-    
-    
